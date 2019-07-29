948637b657e23361f9b8b711e6bb2b5b9e9b2eb7
root <root@DESKTOP-2KU5GC4.localdomain>
2019-07-29 22:02:23 +0300

    added user login for the admin

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var router = express.Router();
-
+var login = require("../../config");
+var locus = require("locus");
 //root route
 router.get("/", function(req, res) {
   // res.sendFile(path.join(__dirname, "/public", "index.html"));
-  res.render("index");
+  res.render("login");
 });
 
+router.get("/login", function(req, res) {
+  if (
+    req.query.username === login.admin.username &&
+    req.query.password === login.admin.password
+  )
+    res.render("index");
+});
 module.exports = router;
